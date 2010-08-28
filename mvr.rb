0a2cf7650f59f73c62e2ad18ea30432c6d8c4419
Alan Andrade <gallito7@gmail.com>
2010-08-27 23:41:07 -0500

    Se agrega una clase DOC que contiene todo lo necesario para representar un documento. Hace falta reducir repeticion de codigo.

--- mvr.rb
+++ mvr.rb
@@ -1,18 +1,64 @@
 require 'load_file.rb'
 
+class Doc
+  attr_accessor :index, :title, :author, :bibliography, :content
+end
+
 file = load_file('cranfield/cran.all.1400')
 
-regexp = /^\.([A-Z]) (\d*)|^\.([A-Z])|(^[^\.].*)/
+parseo_x_regexp = /^\.([A-Z]) (\d*)|^\.([A-Z])|(^[^\.].*)/
+
 # $1 Existe un indice
 # $2 Numero de Indice
 # $3 Atributo
 # $4 Contenido de Atributo
 
-### until file.eof? do
-800.times do
-  if file.gets =~ regexp
-    print "Index: #{$2}\n" if $1 and $2
-    print "Atributo: #{$3}\n" if $3
-    print "\t#{$4}\n" if $4
-  end  
+@docs = []
+until file.eof? do
+#180.times do
+  if file.gets =~ parseo_x_regexp
+    
+    if $1 and $2
+      @docs << @doc if @doc
+      @doc = Doc.new();
+      @doc.index = $2
+    end
+    
+    # ESTE codigo es muuuyyy repetitivo. Se puedo arreglar usando Metaprogramacion, pero no me salia :S hahaha.
+    
+    if $3
+      case $3
+        when 'T'
+          @temp_attr = 'T'
+        when 'A'
+          @temp_attr = 'A'
+        when 'B'
+          @temp_attr = 'B'
+        when 'W'
+          @temp_attr = 'W'
+      end         
+    end
+    
+    if $4
+      if @temp_attr == 'T'
+        @doc.title = '' if @doc.title.nil?
+        @doc.title        <<  $4 
+      end
+      if @temp_attr == 'A'
+        @doc.author = '' if @doc.author.nil?
+        @doc.author       <<  $4
+      end
+      if @temp_attr == 'B'
+        @doc.bibliography = '' if @doc.bibliography.nil?
+        @doc.bibliography <<  $4
+      end
+      if @temp_attr == 'W'
+        @doc.content      = '' if @doc.content.nil?
+        @doc.content      <<  $4        
+      end
+    end
+    
+  end  # file.gets
 end
+print @docs
+
