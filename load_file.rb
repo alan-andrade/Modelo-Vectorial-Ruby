7b6121524d655a6afa826c10a8d301f16c22cebb
Alan Andrade <gallito7@gmail.com>
2010-08-28 11:55:58 -0500

    Se intento separar en componentes, aun falta mucho trabajo en eso. Solo mejoras a la version anterior.

--- load_file.rb
+++ load_file.rb
@@ -1,3 +1,5 @@
 def load_file(file_name)
-  file = open(file_name.to_s)
+  size = File.size(file_name.to_s)
+  print "File: #{file_name.to_s}\nWeight: #{((size/1024).to_f/1024).to_f} Mb\nLoading...\n"
+  open(file_name.to_s)
 end
--- mvr.rb
+++ mvr.rb
@@ -1,63 +1,12 @@
 require 'load_file.rb'
-require 'builder'
+require 'Doc.rb'
+require 'Parser.rb'
 
-class Doc
-  #attr_accessor :index, :title, :author, :bibliography, :content, :terms
-  
-  def initialize
-    @attributes = {}
-  end
-  
-  def method_missing(name, *args)
-    method = name.to_s.downcase
-    if method =~ /=$/
-      @attributes[method.chop] = args[0]
-    elsif method =~ /<<$/
-      method = method.chop.chop
-      @attributes[method] = "" if send(method).nil?
-      @attributes[method] << (args[0] || "")
-    else
-      @attributes[method]
-    end
-  end
-end
-
+## Sirve para estos dos conjuntos de archivos. 'Cranfield' y 'Medline'.
+## TODO: Probar con otras coleccionees de datos.
 
 file = load_file('cranfield/cran.all.1400')
+#file = load_file('med/MED.ALL')
 
-parseo_x_regexp = /^\.([A-Z]) (\d*)|^\.([A-Z])|(^[^\.].*)/
-
-# $1 Existe un indice
-# $2 Numero de Indice
-# $3 Atributo
-# $4 Contenido de Atributo
-
-@docs = []
-#until file.eof? do
-180.times do
-  if file.gets =~ parseo_x_regexp
-    
-    if $1 and $2
-      @docs << @doc if @doc
-      @doc = Doc.new();
-      @doc.send ($1.to_s << "=").to_sym, $2
-    end
-        
-    if $3
-      @doc.send ($3.to_s << "<<").to_sym, $4
-      @doc.temp = $3.to_s.downcase
-    end
-    
-    if $4
-      @doc.send (@doc.temp + "<<").to_sym, $4
-    end
-    
-  end  # file.gets
-end
-puts
-
-for doc in @docs  
-  doc.terms = doc.w.to_s.gsub(/\W+/, ' ').split(' ').uniq.to_s
-end
-
-print @docs.first
+@docs = parse(file)
+puts @docs.first
